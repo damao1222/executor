0cb063e221113ec0b1a160adbfff1b992ba1bb9f
xfali <damao1222@live.com>
2021-01-15 21:20:01 +0800

    add lazyFixBufExecutor

--- executor_steal.go
+++ executor_steal.go
@@ -11,15 +11,13 @@ import (
 	"time"
 )
 
-type WorkStealingExecutor struct {
-	initSize      int
-	runnerSize    int
-	bufSize       int
-	stealInterval time.Duration
-	runners       []TaskRunner
-	curIndex      int
-	runnerLock    sync.Mutex
-	stopCh        chan struct{}
+type LazyFixBUfExecutor struct {
+	initSize   int
+	runnerSize int
+	bufSize    int
+	runners    []TaskRunner
+	curIndex   int
+	runnerLock sync.Mutex
 }
 
 //创建一个固定大小的协程池
@@ -27,14 +25,12 @@ type WorkStealingExecutor struct {
 //taskBufSize： 任务缓冲大小
 //需要注意的是，可以成功加入协程池的任务数量为 size * taskBufSize
 //并且当第一个协程池中的协程taskBufSize被填满之前，不会尝试使用协程池中的其他协程
-func NewWorkStealingExecutor(opts ...ExecutorOpt) *WorkStealingExecutor {
-	ex := &WorkStealingExecutor{
-		initSize:      DefaultInitialGorutineSize,
-		runnerSize:    DefaultMaxGorutineSize,
-		bufSize:       DefaultMaxTaskBufSize,
-		stealInterval: DefaultStealInterval,
-		curIndex:      0,
-		stopCh:        make(chan struct{}),
+func NewLazyFixBUfExecutor(opts ...ExecutorOpt) *LazyFixBUfExecutor {
+	ex := &LazyFixBUfExecutor{
+		initSize:   DefaultInitialGorutineSize,
+		runnerSize: DefaultMaxGorutineSize,
+		bufSize:    DefaultMaxTaskBufSize,
+		curIndex:   0,
 	}
 	for _, opt := range opts {
 		opt(ex)
@@ -45,6 +41,83 @@ func NewWorkStealingExecutor(opts ...ExecutorOpt) *WorkStealingExecutor {
 		go ex.runners[i].Loop()
 	}
 
+	return ex
+}
+
+func (ex *LazyFixBUfExecutor) Run(task Task) error {
+	ex.runnerLock.Lock()
+	defer ex.runnerLock.Unlock()
+
+	if ex.selectRunner(task) {
+		return nil
+	}
+	return errors.New("Run task failed. ")
+}
+
+func (ex *LazyFixBUfExecutor) selectRunner(task Task) bool {
+	for _, runner := range ex.runners {
+		if runner.IsIdle() {
+			return runner.SetTask(task)
+		}
+	}
+	if len(ex.runners) == ex.runnerSize {
+		ex.curIndex++
+		ex.curIndex = ex.curIndex % len(ex.runners)
+
+		return ex.runners[ex.curIndex].SetTask(task)
+	} else {
+		newRunner := NewFIFO(ex.bufSize)
+		//start runner loop
+		go newRunner.Loop()
+		ex.runners = append(ex.runners, newRunner)
+		return newRunner.SetTask(task)
+	}
+}
+
+func (ex *LazyFixBUfExecutor) Stop() {
+	ex.runnerLock.Lock()
+	defer ex.runnerLock.Unlock()
+
+	for _, runner := range ex.runners {
+		runner.Stop()
+	}
+}
+
+func (ex *LazyFixBUfExecutor) setInitialGorutineSize(size int) {
+	ex.initSize = size
+}
+
+func (ex *LazyFixBUfExecutor) setMaxGorutineSize(size int) {
+	ex.runnerSize = size
+}
+
+func (ex *LazyFixBUfExecutor) setTaskBufSize(size int) {
+	ex.bufSize = size
+}
+
+func (ex *LazyFixBUfExecutor) setStealInterval(interval time.Duration) {
+}
+
+type WorkStealingExecutor struct {
+	LazyFixBUfExecutor
+	stealInterval time.Duration
+	stopCh        chan struct{}
+}
+
+//创建一个固定大小的协程池
+//size： 协程池大小
+//taskBufSize： 任务缓冲大小
+//需要注意的是，可以成功加入协程池的任务数量为 size * taskBufSize
+//并且当第一个协程池中的协程taskBufSize被填满之前，不会尝试使用协程池中的其他协程
+func NewWorkStealingExecutor(opts ...ExecutorOpt) *WorkStealingExecutor {
+	ex := &WorkStealingExecutor{
+		LazyFixBUfExecutor: *NewLazyFixBUfExecutor(opts...),
+		stealInterval:      DefaultStealInterval,
+		stopCh:             make(chan struct{}),
+	}
+	for _, opt := range opts {
+		opt(ex)
+	}
 	go func() {
 		ticker := time.NewTicker(ex.stealInterval)
 		defer ticker.Stop()
@@ -58,38 +131,6 @@ func NewWorkStealingExecutor(opts ...ExecutorOpt) *WorkStealingExecutor {
 		}
 	}()
 	return ex
-}
-
-func (ex *WorkStealingExecutor) Run(task Task) error {
-	ex.runnerLock.Lock()
-	defer ex.runnerLock.Unlock()
-
-	//runner := ex.runners[i]
-	runner := ex.selectRunner()
-	if runner != nil && runner.SetTask(task) {
-		return nil
-	}
-	return errors.New("All Runners are busy. ")
-}
-
-func (ex *WorkStealingExecutor) selectRunner() TaskRunner {
-	for _, runner := range ex.runners {
-		if runner.IsIdle() {
-			return runner
-		}
-	}
-	if len(ex.runners) == ex.runnerSize {
-		ex.curIndex++
-		ex.curIndex = ex.curIndex % len(ex.runners)
-
-		return ex.runners[ex.curIndex]
-	} else {
-		newRunner := NewFIFO(ex.bufSize)
-		//start runner loop
-		go newRunner.Loop()
-		ex.runners = append(ex.runners, newRunner)
-		return newRunner
-	}
 }
 
 func (ex *WorkStealingExecutor) doSteal() {
@@ -112,24 +153,7 @@ func (ex *WorkStealingExecutor) doSteal() {
 func (ex *WorkStealingExecutor) Stop() {
 	close(ex.stopCh)
 
-	ex.runnerLock.Lock()
-	defer ex.runnerLock.Unlock()
-
-	for _, runner := range ex.runners {
-		runner.Stop()
-	}
-}
-
-func (ex *WorkStealingExecutor) setInitialGorutineSize(size int) {
-	ex.initSize = size
-}
-
-func (ex *WorkStealingExecutor) setMaxGorutineSize(size int) {
-	ex.runnerSize = size
-}
-
-func (ex *WorkStealingExecutor) setTaskBufSize(size int) {
-	ex.bufSize = size
+	ex.LazyFixBUfExecutor.Stop()
 }
 
 func (ex *WorkStealingExecutor) setStealInterval(interval time.Duration) {
