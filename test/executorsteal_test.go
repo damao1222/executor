8220df57f7adddb3c09f277e11d0b42114af5606
xfali <damao1222@live.com>
2021-02-03 22:11:30 +0800

    增加测试用例

--- test/executorsteal_test.go
+++ test/executorsteal_test.go
@@ -6,8 +6,11 @@
 package test
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/xfali/executor"
+	"runtime"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -38,4 +41,51 @@ func TestStealExecutor(t *testing.T) {
 	})
 
 	<-time.After(5 * time.Second)
+}
+
+func TestStealExecutorSteal(t *testing.T) {
+	executor := executor.NewWorkStealingExecutor(
+		executor.SetInitialGorutineSize(2),
+		executor.SetMaxGorutineSize(2),
+		executor.SetStealInterval(100*time.Millisecond))
+
+	now := time.Now()
+	err := executor.Run(func() {
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("func1 done ", getGID(), " time ", time.Since(now) / time.Millisecond)
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = executor.Run(func() {
+		time.Sleep(3 * time.Second)
+		fmt.Println("func2 done ", getGID(), " time ", time.Since(now) / time.Millisecond)
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = executor.Run(func() {
+		time.Sleep(time.Second)
+		fmt.Println("func3 done ", getGID(), " time ", time.Since(now) / time.Millisecond)
+	})
+
+	err = executor.Run(func() {
+		time.Sleep(time.Second)
+		fmt.Println("func4 done ", getGID(), " time ", time.Since(now) / time.Millisecond)
+	})
+
+	<-time.After(5 * time.Second)
+}
+
+func getGID() uint64 {
+	b := make([]byte, 64)
+	b = b[:runtime.Stack(b, false)]
+	b = bytes.TrimPrefix(b, []byte("goroutine "))
+	b = b[:bytes.IndexByte(b, ' ')]
+	n, _ := strconv.ParseUint(string(b), 10, 64)
+	return n
 }
